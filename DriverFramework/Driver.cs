62111070664c0f8522a2f1aaab0b7145ac52eb33
unknown <peter.hughes@traveljunction.com>
2017-08-10 22:20:02 +0100

    amend driver

--- DriverFramework/Driver.cs
+++ DriverFramework/Driver.cs
@@ -179,45 +179,7 @@ namespace DriverFramework
 
 
         private static void SetCapabilities(DesiredCapabilities capabilities, string profile = null, string environment = null)
-        {            
-            //NameValueCollection caps = ConfigurationManager.GetSection("capabilities/" + profile) as NameValueCollection;
-            //NameValueCollection settings = ConfigurationManager.GetSection("environments/" + environment) as NameValueCollection;
-
-            //DesiredCapabilities capability = new DesiredCapabilities();
-
-            //foreach (string key in caps.AllKeys)
-            //{
-            //    capability.SetCapability(key, caps[key]);
-            //}
-
-            //foreach (string key in settings.AllKeys)
-            //{
-            //    capability.SetCapability(key, settings[key]);
-            //}
-
-            //String username = Environment.GetEnvironmentVariable("james@clicksco.com");
-            //if (username == null)
-            //{
-            //    username = ConfigurationManager.AppSettings.Get("user");
-            //}
-
-            //String accesskey = Environment.GetEnvironmentVariable("7Y@mo4Dei2,VYvp%vQQc7RQfFh}Mgc*ffutwKHdKdB22&RmTcH");
-            //if (accesskey == null)
-            //{
-            //    accesskey = ConfigurationManager.AppSettings.Get("key");
-            //}
-
-            //capability.SetCapability("browserstack.user", username);
-            //capability.SetCapability("browserstack.key", accesskey);
-
-            ////if (capability.GetCapability("browserstack.local") != null && capability.GetCapability("browserstack.local").ToString() == "true")
-            ////{
-            ////    browserStackLocal = new Local();
-            ////    List<KeyValuePair<string, string>> bsLocalArgs = new List<KeyValuePair<string, string>>() {
-            ////    new KeyValuePair<string, string>("key", accesskey)};
-
-            ////    browserStackLocal.start(bsLocalArgs);
-            ////}
+        {  
 
 
             string[] BrowserStackCredentials = { "browserstack.user", "****", "browserstack.key", "******" };
